ec5f36b36388a10adf604c358a5db7c90254250b
Jay Wilson Jr <info@jaywilsonjr.com>
2014-09-11 17:48:07 -0400

    Initiated Github API using jQuery and Underscore

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-$('.imageHolder').append('<img src="' + user.avatar_url + '">');
-$('.imageHolder').append('<h1>' + user.name + '</h1>');
-$('.imageHolder').append('<h3>' + user.login + '</h3>');
-$('.infoArea').append('<span>' + user.location + '</span>');
-$('.infoArea').append('<span>' + user.email + '</span>');
-$('.infoArea').append('<span>' + user.blog + '</span>');
-$('.infoArea').append('<span>' + user.created_at + '</span>');
+function renderTemplate(templateId, model, container) {
+  var templateString = $(templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderTemplate = templateFunction(model);
+  $(container).append(renderTemplate);
+};
+
+
+$.getJSON('https://api.github.com/users/jaywilsonjr').done(function(user) {
+  renderTemplate('#sidebarImageSection', user, '.imageHolder');
+});
