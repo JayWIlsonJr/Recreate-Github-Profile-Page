3ce8731bf4b990bd9be9036c1053f125b5b2cfde
Jay Wilson Jr <info@jaywilsonjr.com>
2014-09-15 07:12:30 -0400

    implemented repos API, styled data also

--- main.js
+++ main.js
@@ -8,8 +8,16 @@ function renderTemplate(templateId, model, container) {
 
 $.getJSON('https://api.github.com/users/jaywilsonjr').done(function(user) {
       $.getJSON('https://api.github.com/users/jaywilsonjr/starred').done(function(starred) {
+            $.getJSON('https://api.github.com/users/jaywilsonjr/repos').done(function(repo) {
           user.starred = starred.length;
           renderTemplate('#sidebarImageSection', user, '.imageHolder');
-          renderTemplate('#header', user, '.nav_two_avatar');   
+          renderTemplate('#header', user, '.nav_two_avatar');
+          
+
+          _.each(repo, function(item){
+            console.log(item.name)
+            renderTemplate('#repos', item, '.repos_area')
+          });
+          }); 
       });
 });
